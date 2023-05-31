2998ac83bafee057422a7cb07cfbd223907a1aa0
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-05-31 14:39:35 -0400

    complete basic mailchimpform component

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -1,9 +1,15 @@
 import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 function MailchimpForm() {
+    const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
     return(
         <section>
-
+            <MailchimpSubscribe 
+                url={postUrl}
+                render={({ subscribe, status, message }) => {
+                    <></>
+                }}
+            />
         </section>
     );
 }
