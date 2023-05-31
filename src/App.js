e24c52939cdf0b3bb4b47add481128f2ca9583f7
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-05-31 15:12:06 -0400

    import footer component into App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
+import Footer from './components/Footer';
 import Projects from './components/Projects';
 
 
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
