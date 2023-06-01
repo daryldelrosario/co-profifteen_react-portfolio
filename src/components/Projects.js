5c746f5c77ad7aa8557a0fdbe832f01de9f04657
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-01 08:44:46 -0400

    create projectcard component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,26 @@
-import { Container, Row, Col, Tab } from 'react-bootstrap';
+import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
+import ProjectCard from './ProjectCard';
 import projImg1 from '../assets/img/project-img1.png';
 import projImg2 from '../assets/img/project-img2.png';
 import projImg3 from '../assets/img/project-img3.png';
+import colorSharp2 from '../assets/img/color-sharp2.png';
 
 function Projects() {
-    const project = [
+    const projects = [
         {
             title: "Business Startup",
             description: "Design & Development",
             imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
         },
         {
             title: "Business Startup",
@@ -18,22 +30,12 @@ function Projects() {
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: projImg1,
+            imgUrl: projImg2,
         },
         {
             title: "Business Startup",
             description: "Design & Development",
-            imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
+            imgUrl: projImg3,
         },
     ]
 
@@ -64,7 +66,10 @@ function Projects() {
                                         {
                                             projects.map((project, index) => {
                                                 return (
-                                                    <p>{project.title}</p>
+                                                    <ProjectCard 
+                                                        key={index}
+                                                        {...project}
+                                                    />
                                                 )
                                             })
                                         }
@@ -81,6 +86,7 @@ function Projects() {
                     </Col>
                 </Row>
             </Container>
+            <img className="background-image-right" src={colorSharp2}/>
         </section>
     );
 }
