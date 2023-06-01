d62edd1498cc381c38e819e7919aff15cdb71eba
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-01 19:16:07 -0400

    update project component with animate.css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,8 @@ import projImg1 from '../assets/img/project-img1.png';
 import projImg2 from '../assets/img/project-img2.png';
 import projImg3 from '../assets/img/project-img3.png';
 import colorSharp2 from '../assets/img/color-sharp2.png';
+import TrackVisibility from 'react-on-screen';
+import 'animate.css';
 
 function Projects() {
     const projects = [
@@ -44,10 +46,16 @@ function Projects() {
             <Container>
                 <Row>
                     <Col>
-                        <h2>Projects</h2>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur maiores error omnis voluptate eveniet, temporibus fuga officia. Voluptatum illum magni, quod, labore eum eveniet facilis quas tenetur doloribus quidem in.
-                        </p>
+                        <TrackVisibility partialVisibility>
+                        {({ isVisible }) => 
+                            <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                                <h2>Projects</h2>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur maiores error omnis voluptate eveniet, temporibus fuga officia. Voluptatum illum magni, quod, labore eum eveniet facilis quas tenetur doloribus quidem in.
+                                </p>
+                            </div>
+                        }
+                        </TrackVisibility>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
                             <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                 <Nav.Item>
