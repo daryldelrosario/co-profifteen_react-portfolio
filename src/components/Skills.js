03c613f882031d672edb4f374df15de345facc66
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-01 07:04:44 -0400

    setup skills component

---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+function Skills() {
+    return(
+        <section>
+
+        </section>
+    )
+}
+
+export default Skills;
