32b339d807e30bda0d607db666f2a080a8ab89af
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-01 23:39:59 -0400

    update social links in navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -44,9 +44,9 @@ function NavBar() {
                 </Nav>
                 <span className="navbar-text">
                 <div className="social-icon">
-                    <a href="#"><img src={navIcon1} alt=""/></a>
-                    <a href="#"><img src={navIcon2} alt=""/></a>
-                    <a href="#"><img src={navIcon3} alt=""/></a>
+                    <a href="https://www.linkedin.com/in/daryldelrosario/" target="_blank"><img src={navIcon1} alt="linkedin icon"/></a>
+                    <a href="https://www.facebook.com/darylxdr/" target="_blank"><img src={navIcon2} alt="facebook icon"/></a>
+                    <a href="https://www.instagram.com/dnaxdr9407/" target="_blank"><img src={navIcon3} alt="instagram icon"/></a>
                 </div>
                 <a href="#contact">
                     <button className="vvd">
