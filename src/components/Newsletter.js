3f56b7d2f2454cab221faa4cbbd0ea24f007a0fe
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-04 14:59:18 -0400

    complete basic newsletter component

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -27,18 +27,20 @@ function Newsletter({ onValidated, status, message }) {
                 <Row>
                     <Col lg={12} md={6} xl={5}>
                         <h3>Subscribe To Our Newsletter</h3>
-                        {status === "sending" && <Alert>Sending ...</Alert>}
-                        {status === "error" && <Alert variant="danger">{message}</Alert>}
-                        {status === "success" && <Alert variant="success">{message}</Alert>}
                     </Col>
                     <Col md={6} xl={7}>
-                        <from onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit}>
                             <div className="new-email-bx">
                                 <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address"/>
                                 <button type="submit">Submit</button>
                             </div>
-                        </from>
+                        </form>
                     </Col>
+                    <div className="newsletter-message">
+                    {status === "sending" && <Alert>Sending ...</Alert>}
+                        {status === "error" && <Alert variant="danger">{message}</Alert>}
+                        {status === "success" && <Alert variant="success">{message}</Alert>}
+                    </div>
                 </Row>
             </div>
         </Col>
