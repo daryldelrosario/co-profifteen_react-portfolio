c435089911613de327a2cfb96cfe3b47a15fbe3e
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-06-04 19:09:56 -0400

    complete footer component in full

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,13 +14,13 @@ function Footer() {
                 <Row className="align-item-center">
                     <MailchimpForm />
                     <Col sm={6}>
-                        <img src={logo} alt="logo" />
+                        <a href="#home"><img src={logo} alt="logo" /></a>
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href=""><img src={navIcon1} /></a>
-                            <a href=""><img src={navIcon2} /></a>
-                            <a href=""><img src={navIcon3} /></a>
+                            <a href="https://www.linkedin.com/in/daryldelrosario/" target="_blank"><img src={navIcon1} alt="linkedin icon"/></a>
+                            <a href="https://www.facebook.com/darylxdr/" target="_blank"><img src={navIcon2} alt="facebook icon"/></a>
+                            <a href="https://www.instagram.com/dnaxdr9407/" target="_blank"><img src={navIcon3} alt="instagram icon"/></a>
                         </div>
                         <p>Daryl del Rosario &#169; {currentYear}. All Rights Reserved</p>
                     </Col>
